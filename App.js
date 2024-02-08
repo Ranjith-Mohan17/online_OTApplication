f9b1f238a2996bd083b91ef964c1aa6aa78730d9
Ranjith-Mohan17 <159436917+Ranjith-Mohan17@users.noreply.github.com>
2024-02-08 17:09:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is my new application</h1>
+      <h3>build your logic here</h3>
+    </div>
+  );
+}
+
+export default App;
